35a172373f54bb10cf8c93e1b0d65ae85a1c89b3
Elec Dash Tron <leon@elec-tron.org>
2020-04-27 12:31:27 +1000

    Update ESP32_MEMSMic.ino

--- ESP32_MEMSMic.ino
+++ ESP32_MEMSMic.ino
@@ -19,7 +19,7 @@ i2s_config_t i2s_config = {
       mode: (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
       sample_rate: 44100,
       bits_per_sample: I2S_BITS_PER_SAMPLE_32BIT,
-      channel_format: I2S_CHANNEL_FMT_ONLY_RIGHT,
+      channel_format: I2S_CHANNEL_FMT_ONLY_LEFT,
       communication_format: (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_LSB),
       intr_alloc_flags: ESP_INTR_FLAG_LEVEL1,
       dma_buf_count: 8,
